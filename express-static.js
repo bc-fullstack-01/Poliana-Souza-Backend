2b3a489ee538b947ecb8022aa31f148db6e7d557
PolianaSouza <polysantos88@gmail.com>
2022-07-05 17:51:08 -0300

    Aprendendo API Rest

--- express-static.js
+++ express-static.js
@@ -1,13 +1,59 @@
-const fs = require('fs')
-const path = require('path')
 const express = require('express')
 const app = express()
+const router = express.Router();
 
-app.get('/', function (req, res) {
-    const content = fs.readFileSync(path.join(__dirname,'static', 'index.html'), 'utf-8')
-    res.send(content)
-});
+app.use('/', express.static(__dirname + "/static"))
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}))
+
+router.route("/")
+    .all((req, res, next) => {
+        console.log(`Request from : ${req.originalUrl}`)
+        console.log(`Request type : ${req.method}`)
+        console.log(`Request params : ${req.params}`)
+        next();
+    })
+    .get((req, res) => {
+        console.log(req.params)
+        res.send(JSON.stringify([]))
+        res.status(200)
+        res.end()
+    })
+    .post((req, res) => {
+        console.log(req.body)
+        res.status(201)
+        res.end()
+    })
+
+router
+    .param('id', (req, res, next, id) => {
+        console.log(`Request from : ${req.params}`)
+        console.log(`Request type : ${req.method}`)
+        console.log(`Request id : ${id}`)
+        next()
+    })
+    .route("/:id")
+    .get((req, res) => {
+        console.log(req.params)
+        res.send(JSON.stringify({}))
+        res.status(200)
+        res.end()
+    })
+    .put((req, res) => {
+        console.log(req.body)
+        res.status(203)
+        res.end()
+    })
+    .delete((req, res) => {
+        res.status(203)
+        res.end()
+    })
+
+app.use('/posts', router)
 
 app.listen(4000, () => {
-    console.log('server listen on http://localhost:4000')
-});
+    console.log('Server listen on http://localhost:4000')
+})
